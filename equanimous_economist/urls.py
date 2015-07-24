ffb3fb925b1e962ffa84309c573305269cef0f3f
Zachary R Smith <ZacharySmith4989@gmail.com>
2015-07-24 01:40:04 +0000

    Add 'Hello' homepage.

--- equanimous_economist/urls.py
+++ equanimous_economist/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'equanimous_economist.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', include('main.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
